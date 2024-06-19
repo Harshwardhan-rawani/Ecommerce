5fb84b847c856196fa22838caa262d27cfedbac2
Harshwardhan <harshwardhan.rawani@gmail.com>
2024-06-19 18:31:14 +0530

    url

--- server/index.js
+++ server/index.js
@@ -19,18 +19,14 @@ const model = require("./model/signup")
 const homeitems = require("./model/Homeitems.js")
 const cart = require("./model/cartpost.js")
 
-const corsOptions = {
-    origin : "https://ecommerce-coral-five-60.vercel.app/"
-    ,methods : "GET, POST, PUT, DELETE , PATCH , HEAD",
-    credentials : true
-}
+
 //connection
 createConnection(process.env.MONOGODB)
 .then(()=>{console.log("mongodb connected")}).catch(()=>{console.log("error")});
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.static(path.join(__dirname)))
 
 app.use("/signup",Signuproute_router)
